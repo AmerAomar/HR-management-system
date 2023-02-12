5fac7a6e362df415632f3d041ea4c1ab6cfe419f
AmerAomar <ameralamriii87@gmail.com>
2023-02-12 07:34:00 +0000

    add elemnts to the html via the app.js file

---
+++ js/app.js
@@ -0,0 +1,70 @@
+function employee(emId,fullname,Department,level,salary){
+    this.employeeId=emId;
+    this.fullName=fullname;
+    this.department=Department;
+    this.level=level;
+    this.salary=salary;
+}
+
+
+employee.prototype.calc=function(){
+    let min;
+    let max;
+    if(this.level==="Senior")
+    {
+        min=1500;
+        max=2000;
+    }
+    else if(this.level==="Mid-Senior")
+    {
+        min=1000;
+        max=1500;
+    }
+    else if(this.level==="Junior")
+    {
+        min=500;
+        max=1000
+    }
+    let ranSal;
+    ranSal= Math.floor(Math.random() * (max - min + 1)) + min;
+    let taxSal;
+    this.salary=ranSal - (ranSal * 0.075);
+    return taxSal;
+
+}
+
+employee.prototype.render = function() {
+    let employeeElement = document.createElement('div');
+    employeeElement.classList.add('employee');
+    employeeElement.innerHTML = `Employee name is: ${this.fullName} and the salary is: ${this.salary}`;
+    document.body.appendChild(employeeElement);
+ }
+  
+
+let ghazi=new employee(1000,"Ghazi Samer","Administration","Senior",1500);
+let lina= new employee(1001,"lina ali","Finance","Senior",1500);
+let tamara= new employee(1002,"Tamara Ayoub","Marketing","Senior",1500);
+let safi= new employee(1003,"Safi Walid","Administration","Mid-Senior",1000);
+let omar= new employee(1004,"Omar Zaid","Development","Senior",1500);
+let rana= new employee(1005,"Rana Saleh","Development","Junior",500);
+let hadi= new employee(1006,"Hadi Ahmad","Finance","Mid-Senior",1000);
+ghazi.calc();
+lina.calc();
+tamara.calc();
+safi.calc();
+omar.calc();
+rana.calc();
+hadi.calc();
+
+ghazi.render();
+lina.render();
+tamara.render();
+safi.render();
+omar.render();
+rana.render();
+hadi.render();
+
+
+
+
+
